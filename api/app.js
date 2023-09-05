6147a9340dd61de96d1c1028c15e6d03cee6a344
GodwinEgo <godwinehile04@gmail.com>
2023-09-05 20:51:48 +0100

    first

--- api/app.js
+++ api/app.js
@@ -1,2 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+
+dotenv.config();
+const port = process.env.PORT || 5001;
+const app = express();
+app.use(express.json());
+
+app.listen(, (req, res) => {
+    res.send(`App is run`)
+} );
