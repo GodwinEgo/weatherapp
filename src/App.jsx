acd7b774d34dfade2741a334dcc3280d95995caf
GodwinEgo <godwinehile04@gmail.com>
2023-09-05 21:04:02 +0100

    axios-fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [message, setMessage] = useState("");
+
+  const fetchMessage = () => {
+    axios
+      .get("http://localhost/5000")
+      .then((response) => {
+        setMessage(`Hello ${response.data.message}`);
+      })
+      .catch((error) => {
+        console.error("Error fetching message: ", error);
+      });
+  };
   return (
     <div>
       <h1 className="text-center">Hello World</h1>
+      <button onClick={fetchMessage}>Click me</button>
+      <p>{message}</p>
     </div>
   );
 };
